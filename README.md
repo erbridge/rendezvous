f6cc8533686148ea989bad8ce22dfd776c8b4047
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-12-12 11:26:42 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Rendezvous
+
+This is a game originally made for Ludum Dare 34 with Maike Vierkant. The theme was **Growing**.
+
+
+## Install
+
+Play [online](http://erbridge.co.uk/rendezvous/).
+
+
+## Develop
+
+Since the site is hosted on GitHub Pages (hence `gh-pages` being the main branch), I use Jekyll when developing it. Any webserver would do, however.
+
+
+### Install
+
+```
+$ bundle install --path="./bundle"
+```
+
+
+### Run
+
+```
+$ bundle exec jekyll serve --watch
+```
---
+++ js/game.js
@@ -0,0 +1,55 @@
+(function() {
+
+'use strict';
+
+// TODO: Specify fonts.
+window.WebFontConfig = {
+  google: {
+    families: [
+      'Lora',
+    ],
+  },
+};
+
+window.startGame = function startGame() {
+  var game = new Phaser.Game(
+    1920, 1080,
+    Phaser.AUTO
+  );
+
+  game.state.add('load', loadState);
+  game.state.add('main', mainState);
+
+  game.state.start('load');
+};
+
+var loadState = {
+  preload: function preload() {
+    this.load.script(
+      'webfont', '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js'
+    );
+  },
+
+  create: function create() {
+    this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+    this.scale.pageAlignHorizontally = true;
+    this.scale.pageAlignVertically   = true;
+
+    // TODO: Load the assets.
+
+    this.load.onLoadComplete.add(function() {
+      this.state.start('main');
+    }, this);
+
+    this.load.start();
+  },
+};
+
+var mainState = {
+  create: function create() {},
+
+  update: function update() {},
+};
+
+})();
